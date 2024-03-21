3795857335017e1fbd77004e350e784fe46436a9
marcotuliosr <50932692+marcotuliosr@users.noreply.github.com>
2024-03-21 17:09:57 -0300

    first-commit-ToDoList

---
+++ lib/todo_list_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  TextEditingController _textEditingController = TextEditingController();
+
+  List<String> tarefas = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Tarefas'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(24),
+        child: Column(
+          children: [
+            TextField(
+              controller: _textEditingController,
+            ),
+            Container(
+              height: 400,
+              child: ListView.separated(
+                separatorBuilder: (context, index) => Divider(),
+                itemCount: tarefas.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(tarefas[index]),
+                    onLongPress: () {
+                      setState(() {
+                        tarefas.removeAt(index);
+                      });
+                    },
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.green,
+            onPressed: () {
+              if (_textEditingController.text.length > 0) {
+                setState(() {
+                  tarefas.add(_textEditingController.text);
+                });
+                _textEditingController.clear();
+              }
+            },
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.green,
+            onPressed: () {
+              setState(() {
+                tarefas = [];
+              });
+              _textEditingController.clear();
+            },
+            child: Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
